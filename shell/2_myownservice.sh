8f5ed644d91596f482fd6b95eab9f564bb5d05ae
aamir-islam <aamirislam38@gmail.com>
2022-05-27 18:13:10 +0530

    first commit

---
+++ shell/2_myownservice.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+while true
+do
+	echo "Hello world "
+	sleep 2
+done
+
---
+++ shell/lamp.sh
@@ -0,0 +1,16 @@
+sudo apt update
+ sudo apt install apache2
+  apache2 -version
+  sudo ufw app list
+  sudo ufw allow 'Apache'
+  sudo ufw status
+  sudo systemctl status apache2
+  hostname –I
+  sudo hostname –I
+  sudo su 
+  sudo apt install php
+  sudo apt install libapache2-mod-php
+  sudo systemctl restart apache2
+  sudo systemctl status apache2
+  sudo apt install php-mysql
+ 
